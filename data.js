cde158ca7faaa642a6db7fbc6a560353619b873a
Ekaterina Molostvova <eam17@pct.edu>
2025-04-07 14:13:58 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,117 @@
+const items = [
+    {
+        "scientific_name": "Saintpaulia ionantha",
+        "common_name": "African Violet",
+        "genus": "Saintpaulia"
+    },
+    {
+        "scientific_name": "Begonia coccinea",
+        "common_name": "Angel Wing Begonia",
+        "genus": "Begonia"
+    },
+    {
+        "scientific_name": "Asparagus densiflorus",
+        "common_name": "Asparagus Fern",
+        "genus": "Asparagus"
+    },
+    {
+        "scientific_name": "Peperomia obtusifolia",
+        "common_name": "Baby Rubber Plant",
+        "genus": "Peperomia"
+    },
+    {
+        "scientific_name": "Sansevieria trifasciata 'Laurentii'",
+        "common_name": "Becky Snake Plant",
+        "genus": "Sansevieria"
+    },
+    {
+        "scientific_name": "Philodendron hederaceum 'Brasil'",
+        "common_name": "Brasil Philodendron",
+        "genus": "Philodendron"
+    },
+    {
+        "scientific_name": "Ficus elastica 'Burgundy'",
+        "common_name": "Burgundy Rubber Plant",
+        "genus": "Ficus"
+    },
+    {
+        "scientific_name": "Hoya carnosa 'Krimson Princess'",
+        "common_name": "Krimson Princess",
+        "genus": "Hoya"
+    },
+    {
+        "scientific_name": "Epipremnum aureum 'Manjula'",
+        "common_name": "Manjula Pothos",
+        "genus": "Epipremnum"
+    },
+    {
+        "scientific_name": "Dracaena trifasciata",
+        "common_name": "Mother-in-Law's Tongue",
+        "genus": "Dracaena"
+    },
+    {
+        "scientific_name": "Epipremnum aureum 'Neon'",
+        "common_name": "Neon Pothos",
+        "genus": "Epipremnum"
+    },
+    {
+        "scientific_name": "Araucaria heterophylla",
+        "common_name": "Norfolk Island Pine",
+        "genus": "Araucaria"
+    },
+    {
+        "scientific_name": "Chamaedorea elegans",
+        "common_name": "Parlor Palm",
+        "genus": "Chamaedorea"
+    },
+    {
+        "scientific_name": "Philodendron erubescens 'Pink Princess'",
+        "common_name": "Pink Princess Philodendron",
+        "genus": "Philodendron"
+    },
+    {
+        "scientific_name": "Ficus lyrata",
+        "common_name": "Quercifolia Fig",
+        "genus": "Ficus"
+    },
+    {
+        "scientific_name": "Zamioculcas zamiifolia 'Raven'",
+        "common_name": "Raven ZZ Plant",
+        "genus": "Zamioculcas"
+    },
+    {
+        "scientific_name": "Ficus elastica 'Ruby'",
+        "common_name": "Ruby Rubber Plant",
+        "genus": "Ficus"
+    },
+    {
+        "scientific_name": "Scindapsus pictus",
+        "common_name": "Silver Philodendron",
+        "genus": "Scindapsus"
+    },
+    {
+        "scientific_name": "Ledebouria socialis",
+        "common_name": "Silver Squill",
+        "genus": "Ledebouria"
+    },
+    {
+        "scientific_name": "Chlorophytum comosum",
+        "common_name": "Spider Plant",
+        "genus": "Chlorophytum"
+    },
+    {
+        "scientific_name": "Monstera deliciosa 'Thai Constellation'",
+        "common_name": "Thai Constellation Monstera",
+        "genus": "Monstera"
+    },
+    {
+        "scientific_name": "Schefflera arboricola",
+        "common_name": "Umbrella Tree",
+        "genus": "Schefflera"
+    },
+    {
+        "scientific_name": "Dionaea muscipula",
+        "common_name": "Venus Flytrap",
+        "genus": "Dionaea"
+    }
+]
