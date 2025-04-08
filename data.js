3eb7a347942ef4a551660e9e2cec8f2cc366718e
Ekaterina Molostvova <eam17@pct.edu>
2025-04-08 12:56:55 -0600

    Update data.js

--- data.js
+++ data.js
@@ -123,6 +123,5 @@ const items = [
         "scientific_name": "Monstera deliciosa 'Thai Constellation'",
         "common_name": "Thai Constellation Monstera",
         "genus": "Monstera",
-        "image_url": "https
-::contentReference[oaicite:0]{index=0}
+        "image_url": "https://www.bhg.com/indoor-palm-plants-8553059"
 ]
