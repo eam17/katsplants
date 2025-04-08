98092090b28922f8eb8dee38d534ac38cba7e5bc
Ekaterina Molostvova <eam17@pct.edu>
2025-04-08 12:55:56 -0600

    Update data.js

--- data.js
+++ data.js
@@ -2,116 +2,127 @@ const items = [
     {
         "scientific_name": "Saintpaulia ionantha",
         "common_name": "African Violet",
-        "genus": "Saintpaulia"
+        "genus": "Saintpaulia",
+        "image_url": "https://www.recordnet.com/gcdn/presto/2019/11/11/PSAL/cd54a658-7398-4509-8281-efb7feb6d143-SAL1Brd_12-15-2017_Statesman_1_D004__2017_12_13_IMG_636486351410331226_b_1_1_G8KIPN7B_L1149165959_IMG_636486351410331226_b_1_1_G8KIPN7B.jpg?width=660&height=557&fit=crop&format=pjpg&auto=webp"
     },
     {
         "scientific_name": "Begonia coccinea",
         "common_name": "Angel Wing Begonia",
-        "genus": "Begonia"
+        "genus": "Begonia",
+        "image_url": "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1713479758-81Y4e7YGSBL.jpg?crop=0.873xw:0.873xh;0.0869xw,0.00427xh&resize=980:*"
     },
     {
         "scientific_name": "Asparagus densiflorus",
         "common_name": "Asparagus Fern",
-        "genus": "Asparagus"
+        "genus": "Asparagus",
+        "image_url": "https://images.squarespace-cdn.com/content/v1/5e1a9e93d343ec0c523f4074/8d25be27-fa89-43da-909b-bc2cb3d4e752/Asparagus_Fern_Orange.jpg"
     },
     {
         "scientific_name": "Peperomia obtusifolia",
         "common_name": "Baby Rubber Plant",
-        "genus": "Peperomia"
+        "genus": "Peperomia",
+        "image_url": "https://edsplantshop.com/cdn/shop/files/peperomia-obtusifolia-variegata-baby-rubber-plant-925115.jpg?v=1732259518"
     },
     {
         "scientific_name": "Sansevieria trifasciata 'Laurentii'",
         "common_name": "Becky Snake Plant",
-        "genus": "Sansevieria"
+        "genus": "Sansevieria",
+        "image_url": "https://planetdesert.com/cdn/shop/files/Variegated-Snake-Plant-Sansevieria-laurentii-6-inch_9_1400x.jpg?v=1739474752"
     },
     {
         "scientific_name": "Philodendron hederaceum 'Brasil'",
         "common_name": "Brasil Philodendron",
-        "genus": "Philodendron"
+        "genus": "Philodendron",
+        "image_url": "https://www.plantvine.com/wp-content/uploads/2018/08/Philodendron-Brasil-Heartleaf-Philodendron-Medium-01.jpg"
     },
     {
         "scientific_name": "Ficus elastica 'Burgundy'",
         "common_name": "Burgundy Rubber Plant",
-        "genus": "Ficus"
+        "genus": "Ficus",
+        "image_url": "https://plants.gardeners.com/12100010/Plant/27358/Burgundy_Rubber_Tree"
     },
     {
         "scientific_name": "Hoya carnosa 'Krimson Princess'",
         "common_name": "Krimson Princess",
-        "genus": "Hoya"
+        "genus": "Hoya",
+        "image_url": "https://www.plantcarefully.com/wp-content/uploads/2023/06/hoya-krimson-princess-featured-01.jpg"
     },
     {
         "scientific_name": "Epipremnum aureum 'Manjula'",
         "common_name": "Manjula Pothos",
-        "genus": "Epipremnum"
+        "genus": "Epipremnum",
+        "image_url": "https://flatwithplants.com/wp-content/uploads/2019/09/manjula-pothos-plant.jpg"
     },
     {
         "scientific_name": "Dracaena trifasciata",
         "common_name": "Mother-in-Law's Tongue",
-        "genus": "Dracaena"
+        "genus": "Dracaena",
+        "image_url": "https://www.istockphoto.com/photos/mother-in-laws-tongue"
     },
     {
         "scientific_name": "Epipremnum aureum 'Neon'",
         "common_name": "Neon Pothos",
-        "genus": "Epipremnum"
+        "genus": "Epipremnum",
+        "image_url": "https://www.pinterest.com/pin/685180530809195994/"
     },
     {
         "scientific_name": "Araucaria heterophylla",
         "common_name": "Norfolk Island Pine",
-        "genus": "Araucaria"
+        "genus": "Araucaria",
+        "image_url": "https://www.istockphoto.com/photos/norfolk-island-pine"
     },
     {
         "scientific_name": "Chamaedorea elegans",
         "common_name": "Parlor Palm",
-        "genus": "Chamaedorea"
+        "genus": "Chamaedorea",
+        "image_url": "https://hortology.co.uk/products/chamaedorea-elegans-parlour-palm-house-plant"
     },
     {
         "scientific_name": "Philodendron erubescens 'Pink Princess'",
         "common_name": "Pink Princess Philodendron",
-        "genus": "Philodendron"
+        "genus": "Philodendron",
+        "image_url": "https://www.shutterstock.com/search/pink-princess-philodendron"
     },
     {
         "scientific_name": "Ficus lyrata",
         "common_name": "Quercifolia Fig",
-        "genus": "Ficus"
+        "genus": "Ficus",
+        "image_url": "https://plants.ces.ncsu.edu/plants/ficus-lyrata/"
     },
     {
         "scientific_name": "Zamioculcas zamiifolia 'Raven'",
         "common_name": "Raven ZZ Plant",
-        "genus": "Zamioculcas"
+        "genus": "Zamioculcas",
+        "image_url": "https://www.thespruce.com/types-of-snake-plants-6826350"
     },
     {
         "scientific_name": "Ficus elastica 'Ruby'",
         "common_name": "Ruby Rubber Plant",
-        "genus": "Ficus"
+        "genus": "Ficus",
+        "image_url": "https://www.southernliving.com/pink-plants-8606683"
     },
     {
         "scientific_name": "Scindapsus pictus",
         "common_name": "Silver Philodendron",
-        "genus": "Scindapsus"
+        "genus": "Scindapsus",
+        "image_url": "https://www.thespruce.com/types-of-hoya-7561716"
     },
     {
         "scientific_name": "Ledebouria socialis",
         "common_name": "Silver Squill",
-        "genus": "Ledebouria"
+        "genus": "Ledebouria",
+        "image_url": "https://www.bhg.com/indoor-palm-plants-8553059"
     },
     {
         "scientific_name": "Chlorophytum comosum",
         "common_name": "Spider Plant",
-        "genus": "Chlorophytum"
+        "genus": "Chlorophytum",
+        "image_url": "https://www.bhg.com/indoor-palm-plants-8553059"
     },
     {
         "scientific_name": "Monstera deliciosa 'Thai Constellation'",
         "common_name": "Thai Constellation Monstera",
-        "genus": "Monstera"
-    },
-    {
-        "scientific_name": "Schefflera arboricola",
-        "common_name": "Umbrella Tree",
-        "genus": "Schefflera"
-    },
-    {
-        "scientific_name": "Dionaea muscipula",
-        "common_name": "Venus Flytrap",
-        "genus": "Dionaea"
-    }
+        "genus": "Monstera",
+        "image_url": "https
+::contentReference[oaicite:0]{index=0}
 ]
